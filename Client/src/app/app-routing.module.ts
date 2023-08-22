a694f0ad056b6dcc39bdc761bc46a793cc3badba
mellamanmar <marimelissamc@gmail.com>
2023-08-22 11:14:03 -0500

    Create folders for signin and signup functions

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersComponent } from './users/users.component';
-import { EditUserComponent } from './users/update/edit-user/edit-user.component'
+import { EditUserComponent } from './users/update/edit-user/edit-user.component';
+import { SignupComponent } from './auth/signup/signup.component'
 
 const routes: Routes = [
+  {path: '', component: SignupComponent},
   {path: 'user', component: UsersComponent},
-  {path: 'edit/:id', component: EditUserComponent},
+  // {path: 'edit/:id', component: EditUserComponent},
+  {},
 ];
 
 @NgModule({
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -4,18 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
-import { AuthComponent } from './auth/auth.component';
 import { ForumComponent } from './forum/forum.component';
 import { HttpClientModule } from '@angular/common/http';
 import { EditUserComponent } from './users/update/edit-user/edit-user.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    AuthComponent,
     ForumComponent,
-    EditUserComponent
+    EditUserComponent,
+    SigninComponent,
+    SignupComponent,
+
   ],
   imports: [
     BrowserModule,
