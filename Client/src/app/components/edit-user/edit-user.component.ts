74c373d518c9cfc8bce3617e0c6e7d574ffee8a9
lina <marcecaroc@gmail.com>
2023-08-21 21:21:52 -0500

    change: modifying edit-user.component.ts file to modify users

--- Client/src/app/components/edit-user/edit-user.component.ts
+++ Client/src/app/components/edit-user/edit-user.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-edit-user',
   templateUrl: './edit-user.component.html',
   styleUrls: ['./edit-user.component.css']
 })
-export class EditUserComponent {
+export class EditUserComponent implements OnInit {
+  users: any[] = [];
 
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.userService.getUsers().subscribe(users => {
+      this.users = users;
+    });
+  }
+
+  editUser(user: any) {
+    // Implementar lógica para la edición del usuario
+  }
+
+  deleteUser(userId: string) {
+    // Implementar lógica para eliminar el usuario
+  }
 }
