6b641c537878c969ca1a741e509a34e01917b6d7
lina <marcecaroc@gmail.com>
2023-08-21 21:40:44 -0500

    change: modifying user.service.ts file to define methods

---
+++ Client/src/app/services/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private apiUrl = 'http://localhost:3000/api/users'; // Cambiar la URL según tu configuración backend
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  editUser(userId: string, userData: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${userId}`, userData);
+  }
+
+  deleteUser(userId: string): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${userId}`);
+  }
+}
