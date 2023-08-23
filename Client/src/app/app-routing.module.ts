97e8d48e7cb5bc8da22a4b2750399a54aeaf861f
LuaMartz <luisamartinez030503@gmail.com>
2023-08-23 17:31:07 -0500

    aupdate app component

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ForumComponent } from './forum/forum.component';
+import { HomeComponent } from './home/home.component';
+import { AuthComponent } from './auth/auth.component';
+import { UsersComponent } from './users/users.component';
+import { UsersEditComponent } from './users-edit/users-edit.component';
+import { ForumAdminComponent } from './forum-admin/forum-admin.component';
 
 const routes: Routes = [
-  { path: "login", component: LoginComponent},
-  { path: '', redirectTo: '/posts', pathMatch: 'full' },
-  { path: 'posts', component: ForumComponent },
-  { path: 'create-post', component: ForumComponent },
+  { path: 'home', component: HomeComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'forum', component: ForumComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'users/edit/:id', component: UsersEditComponent },
+  { path: 'forum/admin', component: ForumAdminComponent },
 ];
 
 @NgModule({
--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Client';
+
+export class AppComponent implements OnInit {
+
+  public title = 'Client';
+	public page_title: string;
+
+	constructor() {
+		this.page_title = 'Bienvenido al foro de programación Genius Botsitos';
+	}
+
+	ngOnInit(): void {
+	}
 }
