81010d12a2bb389cef8695b2cd30750ddd1128c2
mellamanmar <marimelissamc@gmail.com>
2023-08-25 15:48:54 -0500

    update create and post list method

--- Client/src/app/posts/create-post/create-post.component.ts
+++ Client/src/app/posts/create-post/create-post.component.ts
@@ -1,25 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PostService } from '../../services/post.service';
+import { tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-create-post',
   templateUrl: './create-post.component.html',
   styleUrls: ['./create-post.component.css']
 })
-export class CreatePostComponent {
-  postData: any = {};
+export class CreatePostComponent implements OnInit {
+  postData = {
+    _id: '',
+    title: '',
+    content: '',
+    username: ''
+  };
 
-  constructor(private postService: PostService) {}
+  constructor(private postService: PostService, private router: Router) {}
+
+  ngOnInit(): void {
+  }
 
   createPost() {
-    this.postService.createPost(this.postData).subscribe(
-      response => {
-        console.log('Post created successfully', response);
-        // Realiza cualquier otra acción después de crear el post
-      },
-      error => {
-        console.error('Error creating post', error);
-      }
-    );
+    this.postService.createPost(this.postData).pipe(
+      tap (postData =>{
+        console.log(this.postData)
+      })
+    ).subscribe(postData => {
+      this.postData = postData
+      this.router.navigate(['signin'])});
   }
 }
